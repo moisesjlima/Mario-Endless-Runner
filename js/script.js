3a0eee35404c521bafd151ee35dcdef4f71c046f
= <=>
2022-12-01 15:42:02 -0300

    Lets Go V2.0

--- js/script.js
+++ js/script.js
@@ -4,65 +4,89 @@ const clouds = document.querySelector('.clouds')
 const gameInfo = document.querySelector('.game-info')
 const gameOver = document.querySelector('.game-over')
 const countJump = document.querySelector('.count-jump')
+const distance = document.querySelector('.distance')
 
-const jump = (e) => {
-  if(e.which == 32) { // 32 (ASCII code for spacebar)
-    mario.classList.add('jump')
-
-    if(count == 5) {
-      countJump.style.color = 'blue'
-      countJump.style.fontSize = '1.2em'
-    }
-
-    if(count == 30) {
-      countJump.style.color = 'orange'
-      countJump.style.fontSize = '1.4em'
-    }
-
-    if(count == 50) {
-      countJump.style.color = 'red'
-      countJump.style.fontSize = '1.8em'
-    }
-    
-    setTimeout(() => {
-      mario.classList.remove('jump')
-    }, 500)
-    
-    gameInfo.remove()
-  }
-}
 
 var count = 1
+var score = 0
+var end = 0
+const jump = (e) => {
+    if (e.which == 32) { // 32 (ASCII code for spacebar)
+        mario.classList.add('jump')
+
+        if (count == 10) {
+            countJump.style.color = 'blue'
+            countJump.style.fontSize = '16px'
+            setTimeout(score += 15, 2000) 
+        }
+
+        if (count == 20) {
+            countJump.style.color = 'orange'
+            countJump.style.fontSize = '18px'
+            setTimeout(score += 30, 2000) 
+        }
+
+        if (count == 30) {
+            countJump.style.color = 'red'
+            countJump.style.fontSize = '20px'
+            setTimeout(score += 50, 2000) 
+        }
+
+        setTimeout(() => {
+            mario.classList.remove('jump')
+        }, 500)
+
+        gameInfo.remove()
+    }
+}
+
+
 const loop = setInterval(() => {
-  const cloudsPosition = clouds.offsetLeft
-  const pipePosition = pipe.offsetLeft
-  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+    const cloudsPosition = clouds.offsetLeft
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
-  if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80 ) {
-    clouds.style.animation = 'none'
-    clouds.style.left = `${cloudsPosition}px`
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        clouds.style.animation = 'none'
+        clouds.style.left = `${cloudsPosition}px`
 
-    pipe.style.animation = 'none'
-    pipe.style.left = `${pipePosition}px`
+        pipe.style.animation = 'none'
+        pipe.style.left = `${pipePosition}px`
 
-    mario.style.animation = 'none'
-    mario.style.bottom = `${marioPosition}px`
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
 
-    mario.src = 'images/game-over.png'
-    mario.style.width = '70px'
-    mario.style.marginLeft = '50px'
+        mario.src = 'images/game-over.png'
+        mario.style.width = '70px'
+        mario.style.marginLeft = '50px'
 
-    gameInfo.remove()
-    count--
-    gameOver.innerHTML = '<h3>Game Over</h3>'
-    
-    clearInterval(loop)
-  }
+        gameInfo.remove()
+        count--
 
-  if (pipePosition >= -10 && pipePosition <= 50) {
-    countJump.innerHTML = `<h3>${count}</h3>`
-    count++
-  }
-}, 76)
+        gameOver.innerHTML = '<h3>Game Over</h3>'
+        score--
+        distance.innerText = `<h4>Distância: ${score}</h4>`
+        end = 1
+
+        clearInterval(loop)
+    } 
+
+    if (pipePosition >= -12 && pipePosition <= 56) {
+        countJump.innerHTML = `<h3>${count}</h3>`
+        count++
+    }
+}, 75)
+
+const scoreLoop = setInterval(() => {
+    updateScore()
+    if (end == 1) {
+        clearInterval(scoreLoop)
+    }
+}, 300)
+
+function updateScore() {
+    score++
+    distance.innerHTML = `<h4>Distância: ${score}</h4>`
+}
 
 var key = document.addEventListener('keydown', jump);
